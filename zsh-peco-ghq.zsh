251a361ebf04aaef749c5170545eb0f135ba58cd
maediy <maeda.dvlp@gmail.com>
2022-11-16 22:32:52 +0900

    tweak

--- zsh-peco-ghq.zsh
+++ zsh-peco-ghq.zsh
@@ -1,10 +1,12 @@
-function peco-src () {
-    local selected_dir=$(ghq list -p | peco --query "$LBUFFER")
-    if [ -n "$selected_dir" ]; then
-        BUFFER="cd ${selected_dir}"
-        zle accept-line
-    fi
-    zle clear-screen
-}
-zle -N peco-src
-bindkey '^g' peco-src
+if (( $+commands[peco] )); then
+    function peco_src () {
+        local selected_dir=$(ghq list -p | peco --query "$LBUFFER")
+        if [ -n "$selected_dir" ]; then
+            BUFFER="cd ${selected_dir}"
+            zle accept-line
+        fi
+        zle clear-screen
+    }
+    zle -N peco_src
+    bindkey '^g' peco_src
+fi
